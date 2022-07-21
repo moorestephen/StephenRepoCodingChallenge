b483a7d65e0e3204348a100f55659a25e30e6567
paul <paul@localhost>
2022-07-21 17:00:30 -0600

    added my branch

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
+			root.getChildren().add(new Label("Paul's Branch"));
 			root.getChildren().add(new Label("Paul's Label"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
